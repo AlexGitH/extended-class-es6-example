a02e26f3f3c5b4f66568b97794f3a277810f097c
AlexP <alexander.pristavka@gmail.com>
2021-07-11 00:34:32 +0300

    Format function calls; Refactor number formatting code;

--- clock.js
+++ clock.js
@@ -3,33 +3,27 @@ class Clock {
     this.template = template;
   }
   render() {
+    const format = x=>( x < 10 ? '0' : '' ) + x;
     const date = new Date();
-    // TODO: move to formatDigit method
-    // const format = x=>( x < 10 ? '0' : '' ) + x;
 
     let hours = date.getHours();
-    if (hours < 10) hours = '0' + hours;
-
     let mins = date.getMinutes();
-    if (mins < 10) mins = '0' + mins;
-
     let secs = date.getSeconds();
-    if (secs < 10) secs = '0' + secs;
 
     const output = this.template
-      .replace('h', hours)
-      .replace('m', mins)
-      .replace('s', secs);
+      .replace( 'h', format( hours ) )
+      .replace( 'm', format( mins ) )
+      .replace( 's', format( secs ) );
 
-    console.log(output);
+    console.log( output );
   }
 
   stop() {
-    clearInterval(this.timer);
+    clearInterval( this.timer );
   }
 
   start() {
     this.render();
-    this.timer = setInterval(() => this.render(), 1000);
+    this.timer = setInterval( () => this.render(), 1000 );
   }
 }
